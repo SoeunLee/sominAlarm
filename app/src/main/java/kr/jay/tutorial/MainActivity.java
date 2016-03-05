9817fa0c6686c4ca374ea2557c382c66157bdd82
SoeunLee <soeun2327@gmail.com>
2016-03-05 20:33:48 +0900

    activate off button

--- app/src/main/java/kr/jay/tutorial/MainActivity.java
+++ app/src/main/java/kr/jay/tutorial/MainActivity.java
@@ -3,8 +3,10 @@ package kr.jay.tutorial;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView img_siba;
@@ -15,15 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_off = (Button)findViewById(R.id.btn_off);
+        btn_off.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         btn_re = (Button)findViewById(R.id.btn_re);
+        btn_re.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Snooze", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         img_siba = (ImageView)findViewById(R.id.img);
         img_siba.post(new Runnable() {
             @Override
             public void run() {
-                ((AnimationDrawable)img_siba.getBackground()).start();
+                ((AnimationDrawable) img_siba.getBackground()).start();
             }
         });
+
     }
 }
