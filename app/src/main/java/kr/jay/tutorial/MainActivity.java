c6fb6c5ec86414f3560a980bcfb48daf7bf4081d
SoeunLee <soeun2327@gmail.com>
2016-03-05 17:44:35 +0900

    gradle synchronization

--- app/src/main/java/kr/jay/tutorial/MainActivity.java
+++ app/src/main/java/kr/jay/tutorial/MainActivity.java
@@ -3,21 +3,26 @@ package kr.jay.tutorial;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView img;
+    private ImageView img_siba;
+    protected Button btn_off;
+    protected Button btn_re;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_off = (Button)findViewById(R.id.btn_off);
+        btn_re = (Button)findViewById(R.id.btn_re);
 
-        img = (ImageView)findViewById(R.id.img);
-        img.post(new Runnable() {
+        img_siba = (ImageView)findViewById(R.id.img);
+        img_siba.post(new Runnable() {
             @Override
             public void run() {
-                ((AnimationDrawable)img.getBackground()).start();
+                ((AnimationDrawable)img_siba.getBackground()).start();
             }
         });
     }
